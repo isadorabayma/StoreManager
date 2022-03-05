c1774f8c7c66dd77470f4f5c563262c24777e743
isadora <isadora.bayma@gmail.com>
2022-03-04 21:13:19 -0300

    implement minLength fuction

--- middlewares/products.js
+++ middlewares/products.js
@@ -1,4 +1,4 @@
-const { minLength, isRequired } = require('./errorMessages');
+const { minLength, isRequired, minNumber } = require('./errorMessages');
 
 const nameVal = (req, res, next) => {
     const { name } = req.body;
@@ -14,6 +14,21 @@ const nameVal = (req, res, next) => {
     next();
 };
 
+const quantityVal = (req, res, next) => {
+    const { quantity } = req.body;
+
+    if (!quantity) {
+        return res.status(400).json({ message: isRequired('quantity') });
+    }
+
+    if (quantity.length > 0 || Number.isInteger(quantity)) {
+        return res.status(422).json({ message: minNumber('quantity', 1) });
+    }
+
+    next();
+};
+
 module.exports = {
     nameVal,
+    quantityVal,
 };
