3c190cca366ae39a3eb64c6ef144046bbb0e6657
isadora <isadora.bayma@gmail.com>
2022-03-04 21:07:55 -0300

    implement minNumber fuction

--- middlewares/errorMessages.js
+++ middlewares/errorMessages.js
@@ -2,7 +2,10 @@ const isRequired = (field) => `${field} is required`;
 
 const minLength = (field, length) => `${field} length must be at least ${length} characters long`;
 
+const minNumber = (field, length) => `${field} must be greater than or equal to ${length}`;
+
 module.exports = {
     isRequired,
     minLength,
+    minNumber,
 };
